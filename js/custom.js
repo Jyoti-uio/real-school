ae1772e2ba0cdaabe386deca7381e05bde1f1ef9
Jyoti-uio <jyoti.uiodevelopment@gmail.com>
2020-08-31 17:29:32 +0530

    final changes done

---
+++ js/custom.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+    (function($) {
+        $('.french-carol').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+
+            }
+        })
+    })(jQuery);
+
+});
+
+
+$(document).ready(function() {
+    (function($) {
+        $('.attend-carol').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 4
+                }
+
+            }
+        })
+    })(jQuery);
+
+});
+$(document).ready(function() {
+    (function($) {
+        $('.goal-carol').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+
+            }
+        })
+    })(jQuery);
+
+});
+$(document).ready(function() {
+    (function($) {
+        $('.skill-carol').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+
+            }
+        })
+    })(jQuery);
+
+});
